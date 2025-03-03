9c5137de5b98c6deb9f54aa4834b8bd2252afbae
Jay <91969367+Jay-736-Github@users.noreply.github.com>
2025-03-03 16:05:57 +0530

    Update downloadRoutes.js

--- backend/routes/downloadRoutes.js
+++ backend/routes/downloadRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/resume/:file", (req, res) => {
   const address = path.join(__dirname, `../public/resume/${req.params.file}`);
   fs.access(address, fs.F_OK, (err) => {
-    if (err) {
+    if (err) { 
       res.status(404).json({
         message: "File not found",
       });
